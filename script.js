8a7cbc52a263d66865ad9438f128885641ebc62d
Aman Singh <as2211075@gmail.com>
2024-02-23 19:49:35 +0530

    +files
    Added all the files

---
+++ script.js
@@ -0,0 +1,9 @@
+VanillaTilt.init(document.querySelectorAll(".card"),{
+    reverse: true,
+    max: 10,
+    speed: 400,
+    glare: true,
+    "max-glare": .5,
+    scale: 1.07,
+    gyroscope: true,
+})
